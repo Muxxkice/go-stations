e23c3bdcf7e8491da788a66e4ad5fcc7622801c0
Mao Kurihara <mao.kurihara@gmail.com>
2023-11-10 07:56:14 +0900

    station2

--- main.go
+++ main.go
@@ -1,56 +1,65 @@
 package main
 
 import (
-	"log"
-	"os"
-	"time"
-
-	"github.com/TechBowl-japan/go-stations/db"
-	"github.com/TechBowl-japan/go-stations/handler/router"
+  "log"
+  "os"
+  "time"
+  "net/http"
+  "fmt"
+  "github.com/TechBowl-japan/go-stations/db"
+  "github.com/TechBowl-japan/go-stations/handler/router"
 )
 
 func main() {
-	err := realMain()
-	if err != nil {
-		log.Fatalln("main: failed to exit successfully, err =", err)
-	}
+  err := realMain()
+  if err != nil {
+    log.Fatalln("main: failed to exit successfully, err =", err)
+  }
 }
 
 func realMain() error {
-	// config values
-	const (
-		defaultPort   = ":8080"
-		defaultDBPath = ".sqlite3/todo.db"
-	)
+  // config values
+  const (
+    defaultPort   = ":8080"
+    defaultDBPath = ".sqlite3/todo.db"
+  )
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = defaultPort
-	}
+  port := os.Getenv("PORT")
+  if port == "" {
+    port = defaultPort
+  }
 
-	dbPath := os.Getenv("DB_PATH")
-	if dbPath == "" {
-		dbPath = defaultDBPath
-	}
+  dbPath := os.Getenv("DB_PATH")
+  if dbPath == "" {
+    dbPath = defaultDBPath
+  }
 
-	// set time zone
-	var err error
-	time.Local, err = time.LoadLocation("Asia/Tokyo")
-	if err != nil {
-		return err
-	}
+  // set time zone
+  var err error
+  time.Local, err = time.LoadLocation("Asia/Tokyo")
+  if err != nil {
+    return err
+  }
 
-	// set up sqlite3
-	todoDB, err := db.NewDB(dbPath)
-	if err != nil {
-		return err
-	}
-	defer todoDB.Close()
+  // set up sqlite3
+  todoDB, err := db.NewDB(dbPath)
+  if err != nil {
+    return err
+  }
+  defer todoDB.Close()
 
-	// NOTE: 新しいエンドポイントの登録はrouter.NewRouterの内部で行うようにする
-	mux := router.NewRouter(todoDB)
+  // NOTE: 新しいエンドポイントの登録はrouter.NewRouterの内部で行うようにする
+  mux := router.NewRouter(todoDB)
 
-	// TODO: サーバーをlistenする
+  // URLに対応する処理を登録
+  // TODO: サーバーをlistenする
 
-	return nil
+  mux.HandleFunc("/", handler) // ハンドラを登録してウェブページを表示させる
+  http.ListenAndServe(defaultPort, nil)
+
+  return nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello, World")
 }
